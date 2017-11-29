50ed3d55a2eb252d6df9ca47ac0385d77fbc5bd0
ibrahimparvez2 <31848160+ibrahimparvez2@users.noreply.github.com>
2017-11-29 10:47:21 +0000

    Add files via upload

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace FirstTake
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            //routes.MapRoute(
+            //    name:"OppenentQuery",
+            //    url: "Teams/View/{id}/{*query}",
+            //    defaults: new { controller = "Teams", action = "View", id = UrlParameter.Optional }
+            //    );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
